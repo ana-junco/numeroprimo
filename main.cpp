83164b15371944c6208e7e6928adc4cf694d9fce
Ana Junco <anajuncoescobar@gmail.com>
2021-06-20 09:35:41 -0500

    Calcular si el numero es primo o no

--- main.cpp
+++ main.cpp
@@ -5,6 +5,19 @@ int main() {
 //Ejercicio 4:  Se ingresa un entero por teclado. Se desea determinar
 //si el número ingresado es primo o no. Utilice bucles "while".
 
+//1)Inicio
+//2)Leer numero, n=2, band
+//3)Ingresar un numero
+//4)numero
+//5)while (band && n< numero)
+//6)Si(numero % n== 0)
+//6.1) band = false
+//7)caso contrario
+//8) n = n+1
+//9)Imprimir si el numero es primo o no
+//10)Fin
+
+
         int numero, n=2;
         bool band = true;
 
